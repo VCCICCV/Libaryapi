98060d7378af01e1b398d40bc38e6312152999a6
VCCICCV <chaochen497@gmail.com>
2023-04-08 22:22:45 +0800

    first commit

---
+++ src/main/java/com/example/libary/LibaryApplication.java
@@ -0,0 +1,20 @@
+package com.example.libary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class LibaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibaryApplication.class, args);
+    }
+    @GetMapping
+    public String health(){
+        return "success";
+}
+}
---
+++ src/main/java/com/example/libary/entity/User.java
@@ -0,0 +1,9 @@
+package com.example.libary.entity;
+
+/**
+ * @PROJECT_NAME Libary
+ * @AUTHOR VCCICCV
+ * @DATE 2023/4/8 15:38
+ */
+public class User {
+}
